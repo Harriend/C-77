2dede030527b61e9f895ae40e5a2515436b6d4b1
Harriend <88569431+Harriend@users.noreply.github.com>
2021-11-05 12:17:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var first_name="Hrithik";
+var second_name="Roshan";
+var first_no=8;
+var second_no=4;
+
+var full_name=first_name.concat(second_name);
+console.log(full_name);
+
+var add=first_name+second_name;
+console.log(add);
+
+var sqrt=first_no.sqrt(5);
+console.log(sqrt)
